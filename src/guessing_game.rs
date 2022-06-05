36e7f427209758b0abb1ef3acd8df84eb748e4c2
JohnAnon9771 <njoao97710@gmail.com>
2022-06-04 23:47:18 -0300

    ❇ concluded guessing game

---
+++ src/guessing_game.rs
@@ -0,0 +1,33 @@
+use std::cmp::Ordering;
+use std::io;
+
+use rand::Rng;
+
+pub fn exec() -> () {
+    let mut rng = rand::thread_rng();
+    let generated_number = rng.gen_range(0..101);
+
+    loop {
+        println!("Digite um número");
+        
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&generated_number) {
+            Ordering::Less => println!("Muito baixo"),
+            Ordering::Greater => println!("Muito alto"),
+            Ordering::Equal => {
+                println!("Parabéns, você acertou!");
+                break;
+            }
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+mod guessing_game;
+mod introdution;
+
+fn main() {
+    introdution::print("Hello, Mom!");
+    guessing_game::exec();
+}
