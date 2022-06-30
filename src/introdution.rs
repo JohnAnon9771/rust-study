b24d800cd4879040ac26be4283c3ea11e43499eb
JohnAnon9771 <njoao97710@gmail.com>
2022-06-30 01:36:31 -0300

    read cap 3.1 and 3.2

--- src/introdution.rs
+++ src/introdution.rs
@@ -1,3 +1,20 @@
 pub fn print(text: &str) {
     println!("{}", text);
 }
+
+pub fn shadowing() {
+    let x = 5;
+    let x = x + 1;
+    let test: i32;
+
+    // scopes
+    {
+        // intern shadowing
+        let x = x * 2;
+        test = x;
+        println!("inner: {}", x);
+    }
+
+    println!("outer: {}", x);
+    println!("test: {}", test);
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,18 @@ mod guessing_game;
 mod introdution;
 
 fn main() {
+    // variables
+    let _name = "João"; // immutable variable
+    let mut _age = 30; // mutable variable
+    const _PI: f32 = 3.14; // constant variable
+
+    // scalar types
+    // integer types, float types, boolean types, char types
+
+    // compound types
+    // tuple types, array types
+
     introdution::print("Hello, Mom!");
+    introdution::shadowing();
     guessing_game::exec();
 }
